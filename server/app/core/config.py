2817ca2fba90c0353f18bec5657c50d521a1ec25
GomSon-E <gomsonixx@gmail.com>
2026-02-07 19:40:06 +0900

    FEAT: Add DB Connection Setting

--- server/app/core/config.py
+++ server/app/core/config.py
@@ -1,13 +1,19 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
+    #MINIO CONFIGURATION
     MINIO_ENDPOINT: str = "localhost:9000"
     MINIO_ACCESS_KEY: str
     MINIO_SECRET_KEY: str
     MINIO_SECURE: bool
     MINIO_BUCKET_NAME: str
-
     PRESIGNED_URL_EXPIRE_MINUTES: int = 10
+
+    #DATABASE CONFIGURATION
+    DATABASE_URL: str = "postgresql+asyncpg://robot_studio:robot_studio@localhost:5432/robot_studio"
+    JWT_SECRET_KEY: str = "change-this-to-random-secret"
+    JWT_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     
     class Config:
         env_file = ".env"
